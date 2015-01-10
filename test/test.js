36ec299ebbe05842e4c72213595f8ba476343e17
Michael Lange <dingoeatingfuzz@gmail.com>
2015-01-09 22:08:31 -0800

    Add mocha testing framework (using chai)

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('test suite', function() {
+  it('should be using chai', function() {
+    expect(true).to.be.true;
+    expect(false).to.be.false;
+  });
+});
