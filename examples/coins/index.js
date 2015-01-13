73a4aebff2c33e39a876de7a96204226289f411b
Michael Lange <dingoeatingfuzz@gmail.com>
2015-01-12 23:58:47 -0800

    Tweak the GA instance to have working parameters

--- examples/coins/index.js
+++ examples/coins/index.js
@@ -2,9 +2,9 @@ var Coins = require('./coins');
 
 // Create an instance of our Coins genetic algorithm
 var ga = new Coins({
-  populationSize : 250,
-  mutationRate   : 0.2,
-  elitismBias    : 0
+  populationSize : 500,
+  mutationRate   : 0.01,
+  elitismBias    : 0.2
 });
 
 // The match property is undefined until something meets the success criteria
@@ -16,4 +16,11 @@ while(!ga.match) {
 
 console.log('\n\n');
 console.log('Match Found!');
-console.log(ga.match.coins.join(', '));
+console.log(histogram(ga.match.coins));
+
+function histogram(a) {
+  return JSON.stringify(a.reduce(function(h, n) {
+    h[n] ? h[n]++ : h[n] = 1;
+    return h;
+  }, {}), undefined, 2);
+}
